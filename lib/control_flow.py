3ce93941daed1e28de5e9882582bffd41a1559d1
George <georgejoj28@gmail.com>
2023-11-23 10:58:56 +0300

    completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,55 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if username.upper() == 'ADMIN' and password == '12345':
+        return 'Access granted'
+    else:
+        return 'Access denied'
+    
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        response = 'brisk'
+    elif temperature >= 40 and temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else: 
+            print("Error: Division by zero")
+            return None
+    else:
+        print("Invalid operation")
+        return None
+        
+
+
+# print(calculator("**" ,2 , 3))
+
